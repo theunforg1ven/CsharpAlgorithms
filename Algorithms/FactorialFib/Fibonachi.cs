447fb71e2e02efdb23b59353fde9982a944a56a0
TheUnforg1ven <TheUnforg1ven@users.noreply.github.com>
2018-12-26 13:54:59 +0200

    fixed fibonachi algorithm

--- Algorithms/FactorialFib/Fibonachi.cs
+++ Algorithms/FactorialFib/Fibonachi.cs
@@ -12,16 +12,12 @@
                 return FibonachiTwo(num - 1) + FibonachiTwo(num - 2);
         }
 
-        public static int FibonachiThree(int num)
-        {
-            if (num == 0)
-                return 0;
-            else if(num == 1)
-                return 1;
-            else if (num == 2)
-                return 2;
-            else
-                return FibonachiThree(num - 1) + FibonachiThree(num - 2) + FibonachiThree(num - 3);
-        }
+        static public int FibonachiThree(int num)
+		{
+			if (num < 3)
+				return num;
+			else
+				return FibonachiThree(num - 1) + FibonachiThree(num - 2) + FibonachiThree(num - 3);
+		}
     }
 }
