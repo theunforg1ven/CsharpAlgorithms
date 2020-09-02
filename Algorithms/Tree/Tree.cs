d2c401b1b30663dc78907811c595f3b57c736698
TheUnforg1ven <tbatjuk4u@gmail.com>
2020-09-02 22:11:39 +0300

    Added tree class

---
+++ Algorithms/Tree/Tree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays.Tree
+{
+	public class Tree
+	{
+        public TreeNode Insert(TreeNode root, int v)
+        {
+            if (root == null)
+                root = new TreeNode(v);
+
+            else if (v < root.Value)
+                root.Left = Insert(root.Left, v);
+
+            else
+                root.Right = Insert(root.Right, v);
+
+            return root;
+        }
+
+        public void Traverse(TreeNode root)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            Traverse(root.Left);
+            Traverse(root.Right);
+        }
+    }
+}
---
+++ Algorithms/Tree/TreeNode.cs
@@ -0,0 +1,13 @@
+﻿namespace Arrays.Tree
+{
+	public class TreeNode
+	{
+		public int Value { get; set; } 
+
+		public TreeNode Left { get; set; }
+
+		public TreeNode Right { get; set; }
+
+		public TreeNode(int value) => Value = value;
+	}
+}
